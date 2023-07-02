557b7ec7d9fae0080f1a69696c7f38543945cccb
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-07-02 15:35:18 +0700

    Remake the `conio_lt.h`
    This changes turning the `conio_lt.h` from C++ code to C code.
    
    Currently changed:
      * __getch(unsigned int);
      * __whereis_xy(int&, int&);
    
    Function `__getch` now no longer uses boolean type on its parameter,
    instead uses `unsigned int`.

--- conio_lt.h
+++ conio_lt.h
@@ -1,35 +1,38 @@
-/*
+/**
+ * @file conio_lt.h
+ *
+ * Copyright (c) 2023 Ryuu Mitsuki
+ * Authored and developed by Ryuu Mitsuki
+ *
+ * @version  0.1.5, 2 July 2023
+ *
+ * Description
+ * -------------
+ * Similar like 'conio.h', but it's a lite version of 'conio.h'.
+ * I hope this can be useful for your project or something else.
+ *
+ * Made with : Termux
+ * OS type   : Android (aarch64-linux)
+ * Clang ver : 15.0.7
+ * ------------
+ *
+ * List Functions
+ * --------------
+ * :>  getch()
+ * :>  getche()
+ * :>  ungetch()
+ * :>  clrscr()
+ * :>  gotoxy()
+ * :>  wherex()
+ * :>  wherey()
+ * :>  putch()
+ * :>  putchar()
+ * --------------
+ *
+ */
 
-[======== conio_lt.h ========]
-[ Author  : Ryuu Mitsuki     ]
-[ Version : 0.1.2            ]
-[============================]
-
-Description
--------------
-* Similar like 'conio.h', but it's a lite version of 'conio.h'.
-* I hope this can be useful for your project or something else.
-*
-* Made with : Termux
-* OS type   : Android (aarch64-linux)
-* Clang ver : 15.0.7
-------------
-
-List Functions
---------------
-:>  getch()
-:>  getche()
-:>  ungetch()
-:>  clrscr()
-:>  gotoxy()
-:>  wherex()
-:>  wherey()
-:>  putch()
-:>  putchar()
---------------
-
-*/
-
+#ifndef CONIO_LT_H_
+#define CONIO_LT_H_
 
 
 #include <stdio.h>
@@ -37,134 +40,78 @@ List Functions
 #include <fcntl.h>
 #include <termios.h>
 
-#ifndef __CONIO_LITE_H__
-#define __CONIO_LITE_H__
+static const char* __prefix = "\033[";
 
-class __CONIO_LT {
-    private:
-        const char* __prefix = "\033[";
+/**
+ * @brief Reads a single character from the standard input with optional echo.
+ * This function reads a single character from the standard input, allowing
+ * for optional echo of the input character.
+ * It uses the \c <termios.h> header and the \c tcgetattr and \c tcsetattr functions to modify the terminal settings.
+ *
+ * @param  __echo  A flag indicating whether to echo the input character
+ *                 (non-zero value for echo, 0 for no echo).
+ *
+ * @return         Returns the character read from the standard input.
+ *                 Please note, this function returns the character as integer value.
+ *
+ * @since          0.1.0
+ */
+static const int __getch(unsigned int __echo) {
+    struct termios __oldt, __newt;
+    int __c;
 
-        const int __getch(const bool __echo) {
-            struct termios __oldt, __newt;
-            int __c;
+    tcgetattr(STDIN_FILENO, &__oldt);
+    __newt = __oldt;
+    __newt.c_lflag &= ~ICANON;
 
-            tcgetattr(STDIN_FILENO, &__oldt);
-            __newt = __oldt;
-            __newt.c_lflag &= ~ICANON;
+    if (__echo != 0) {
+        __newt.c_lflag &= ECHO;
+    } else {
+        __newt.c_lflag &= ~ECHO;
+    }
 
-            if(__echo) {
-                __newt.c_lflag &= ECHO;
-            } else {
-                __newt.c_lflag &= ~ECHO;
-            }
+    tcsetattr(STDIN_FILENO, TCSANOW, &__newt);
+    __c = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &__oldt);
 
-            tcsetattr(STDIN_FILENO, TCSANOW, &__newt);
-            __c = getchar();
-            tcsetattr(STDIN_FILENO, TCSANOW, &__oldt);
+    return __c;
+}
 
-            return __c;
-        }
+/**
+ * @brief Retrieves the current cursor position on the terminal screen.
+ * This function retrieves the current cursor position on the terminal screen.
+ * It takes two integer references, __x and __y, as output parameters to store
+ * the X and Y coordinates of the cursor, respectively.
+ *
+ * @param[out] __x  A reference to an integer to store the X-coordinate of the cursor.
+ * @param[out] __y  A reference to an integer to store the Y-coordinate of the cursor.
+ *
+ * @since           0.1.0
+ */
+static const void __whereis_xy(int &__x, int &__y) {
+    int in, x = 0, y = 0;
+    printf("%s6n", __prefix);
 
-        const void __whereis_xy(int &__x, int &__y) {
-            int in, x = 0, y = 0;
-            printf("%s6n", __prefix);
-            if(getch() != '\x1B') {
-                return;
-            }
-            else if(getch() != '\x5B') {
-                return;
-            }
+    /* If the character does not same as '\x1b' nor '\x5b',
+     * immediately return and leaving the __x and __y references unchanged
+     */
+    if (__getch(0) != '\x1B') {
+        return;
+    } else if (__getch(0) != '\x5B') {
+        return;
+    }
 
-            while((in = getch()) != ';') {
-                y = y * 10 + in - '0';
-            }
-            while((in = getch()) != 'R') {
-                x = x * 10 + in - '0';
-            }
+    while ((in = __getch(0)) != ';') {
+        y = y * 10 + in - '0';
+    }
 
-            __x = x;
-            __y = y;
-        }
+    while ((in = __getch(0)) != 'R') {
+        x = x * 10 + in - '0';
+    }
 
-    public:
-        // :: Constructor
-        __CONIO_LT() { }
+    /* Store and assign the cursor position */
+    __x = x;
+    __y = y;
+}
 
-        // :: Destructor
-        ~__CONIO_LT() {
-            printf("%sm", __prefix);
-        }
-
-        // :: gotoxy(x, y) -> void
-        const void gotoxy(const int __x, const int __y) {
-            printf("%s%d;%df", __prefix, __y, __x);
-        }
-
-        // :: clrscr() -> void
-        const void clrscr() {
-            /*
-            * "\033[1J" = clear entire screen permanently (cannot scrolled to up)
-            * "\033[2J" = clear entire screen but can be scrolled to up
-            */
-            printf("%s0m%s1J%s1;1f", __prefix, __prefix, __prefix);
-        }
-
-        // :: ungetch(char) -> int
-        const int ungetch(const int __c) {
-           return ungetc(__c, stdin);
-        }
-
-        // :: getch() -> int
-        const int getch() {
-            int __c;
-            __c = this->__getch(false);
-
-            return __c;
-        }
-
-        // :: getche() -> int
-        const int getche() {
-            int __c;
-            __c = this->__getch(true);
-
-            return __c;
-        }
-
-        // :: wherex() -> int
-        const int wherex() {
-            int __x = 0, __y = 0;
-            this->__whereis_xy(__x, __y);
-
-            /* only return x value */
-            return __x;
-        }
-
-        // :: wherey() -> int
-        const int wherey() {
-            int __x = 0, __y = 0;
-            this->__whereis_xy(__x, __y);
-
-            /* only return y value */
-            return __y;
-        }
-
-        // :: putchar() & putch() -> int
-        const int putchar(const int __char) {
-            printf("%c", __char);
-            return (int)__char;
-        }
-} __CONIO_LT;
-
-
-
-#define gotoxy    __CONIO_LT.gotoxy
-#define clrscr    __CONIO_LT.clrscr
-#define ungetch   __CONIO_LT.ungetch
-#define getch     __CONIO_LT.getch
-#define getche    __CONIO_LT.getche
-#define wherex    __CONIO_LT.wherex
-#define wherey    __CONIO_LT.wherey
-#define putch     __CONIO_LT.putchar
-#define putchar   __CONIO_LT.putch
-
-#endif // __CONIO_LITE_H__
+#endif /* CONIO_LT_H_ */
